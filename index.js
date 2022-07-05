d6d3f852222678ba9006dcbf36ce2c517c889c7c
venturecheta <raphaelemecheta3@gmail.com>
2022-07-06 00:19:52 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Enables hamburger button to slide and show menu options on phones
+
+
+function openMenu() {
+    document.body.classList += " menu--open"
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu--open')
+}
